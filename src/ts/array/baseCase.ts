7cc346e2515ce5f60a5330bf6f277aa42d7d73fa
Nicolas <devnicolas.work@gmail.com>
2025-07-14 12:52:20 -0300

    adding new recursive functions

--- src/ts/array/baseCase.ts
+++ src/ts/array/baseCase.ts
@@ -9,3 +9,14 @@ function baseCase(arr: number[]): number {
 
 console.log('case 1', baseCase([1, 2, 3, 4, 5, 6, 7]));
 console.log('case 2', baseCase([1]));
+
+function baseCase2(arr: number[]): number {
+	if (arr.length === 0) return 0;
+
+	arr.pop();
+	return 1 + baseCase2(arr);
+}
+
+console.log('case 1', baseCase2([1, 2, 3]));
+console.log('case 2', baseCase2([1]));
+console.log('case 3', baseCase2([]));
